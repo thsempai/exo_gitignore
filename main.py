2030c542da844539301f17b8f57a66eb3731f00b
Thomas Stassin <thomas.stassin@interface3.be>
2026-06-16 11:12:41 +0200

    change line to data

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("Hello, world!")
 
 with file in open("data.txt"):
     
-    line = file.readline()
-    while(line):
-        print(line)
-        line = file.readline()
+    data = file.readline()
+    while(data):
+        print(data)
+        data = file.readline()
